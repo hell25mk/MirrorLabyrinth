a1b91804a19632f0e959cd3a5880169f849d783a
hell25mk <hell25mk@gmail.com>
2018-06-22 13:52:54 +0900

    [update] 命名変更
    BreakWallをBlockに変更

--- Script/Object/ObjectListh.h
+++ Script/Object/ObjectListh.h
@@ -1,11 +1,13 @@
 #pragma once
 
+//プレイヤー
+#include "Player.h"
+#include "Mirror.h"
+
+//ギミック
 #include "Road.h"
 #include "Wall.h"
 #include "Stairs.h"
 #include "Door.h"
 #include "Key.h"
-#include "BreakWall.h"
-
-#include "Player.h"
-#include "Mirror.h"
+#include "Block.h"
